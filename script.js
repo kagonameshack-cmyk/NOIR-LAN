967edb8e685972afb9345e3f865a5a44cf3700c1
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-05-17 20:26:05 +0300

    script.js

--- script.js
+++ script.js
@@ -16,3 +16,41 @@ window.addEventListener(
     }
 
 });
+
+
+const nav =
+document.querySelector("nav");
+
+window.addEventListener(
+"scroll",
+() => {
+
+    if(window.scrollY > 80){
+
+        nav.classList.add("scrolled");
+
+    }
+
+    else{
+
+        nav.classList.remove("scrolled");
+
+    }
+
+});
+
+/* MOBILE MENU */
+
+const menuToggle =
+document.querySelector(".menu-toggle");
+
+const mobileMenu =
+document.querySelector(".mobile-menu");
+
+menuToggle.addEventListener(
+"click",
+() => {
+
+    mobileMenu.classList.toggle("active");
+
+});
